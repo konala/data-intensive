69199baeaa3beec93e4a8412e1eb493489962df9
konala <konsta.ala-ilomaki@luukku.com>
2017-12-12 18:30:42 +0000

    lb bug fix

--- index.php
+++ index.php
@@ -27,15 +27,16 @@ foreach($endPoints as $endPoint) {
 	
 	$isOnline = @fsockopen($endPoint["IP"], $port, $errno, $errstr, 1);
 	if(!$isOnline) {
-		print $errstr;
+		#print $errstr;
 	} else {
 		$endPoints[$i]["Online"] = "Yes";
 	}
 	$i++;
 	
 }
-
-
+#Debug prints
+#print_r($endPoints);
+#print($regId);
 # Priority EU, NA, AS
 if ($regId == 1) {
 	if($endPoints[0]["Online"] == "Yes") {
@@ -46,19 +47,20 @@ if ($regId == 1) {
 		exit();
 	} elseif($endPoints[2]["Online"] == "Yes"){
 		header("Location: http://40.69.220.19/index2.php"); # AS IP here
+		print "debug";
 		exit();
 	} else {
 		print "All servers down!"; # index.php needs to be run from load balancer server! As this cannot currently be reached.
 	}
 # Priority NA, AS, EU
 } elseif ($regId == 2) {
-	if($endPoints[0]["Online"] == "Yes") {
+	if($endPoints[1]["Online"] == "Yes") {
 		header("Location: http://52.164.184.175/index2.php");
 		exit();
-	} elseif($endPoints[1]["Online"] == "Yes"){
+	} elseif($endPoints[2]["Online"] == "Yes"){
 		header("Location: http://40.69.220.19/index2.php"); 
 		exit();
-	} elseif($endPoints[2]["Online"] == "Yes"){
+	} elseif($endPoints[0]["Online"] == "Yes"){
 		header("Location: http://52.169.151.180/index2.php"); 
 		exit();
 	} else {
@@ -66,13 +68,13 @@ if ($regId == 1) {
 	}
 # Priority AS, NA, EU
 } else {
-	if($endPoints[0]["Online"] == "Yes") {
+	if($endPoints[2]["Online"] == "Yes") {
 		header("Location: http://40.69.220.19/index2.php");
 		exit();
 	} elseif($endPoints[1]["Online"] == "Yes"){
 		header("Location: http://52.164.184.175/index2.php"); 
 		exit();
-	} elseif($endPoints[2]["Online"] == "Yes"){
+	} elseif($endPoints[0]["Online"] == "Yes"){
 		header("Location: http://52.169.151.180/index2.php"); 
 		exit();
 	} else {
